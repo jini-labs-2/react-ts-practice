d1e4a2525c6c66bc53dd3fe7d3dd76d4c7f44644
und-jini <choiyongjin@undershaft.net>
2024-12-14 00:55:36 +0900

    theme変更例

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { ThemeProvider } from 'styled-components';
+import { darkTheme, lightTheme } from './theme';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  background-color: ${(props) => props.theme.colors.background};
+  color: ${(props) => props.theme.colors.text};
+  font-size: ${(props) => props.theme.fontSize.medium};
+  padding: 20px;
+  width: 1024px;
+  height: 100vh;
+  margin: 0 auto;
+`;
+
+const Button = styled.button`
+  background-color: ${(props) => props.theme.colors.primary};
+  color: white;
+  border: none;
+  padding: 10px 15px;
+  border-radius: 5px;
+  font-size: ${(props) => props.theme.fontSize.medium};
+
+  &:hover {
+    background-color: ${(props) => props.theme.colors.secondary};
+  }
+`;
+
 
 function App() {
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
+
   return (
-    <div className='app'>
-      App
-    </div>
+    <>
+      <h1>テーマ変更例</h1>
+      <hr />
+      <ThemeProvider theme={isDarkTheme ? darkTheme : lightTheme}>
+        <Wrapper>
+          <h1>{isDarkTheme ? ' ダークモード ': ' ライトモード '}</h1>
+          <Button
+          onClick={() => setIsDarkTheme(!isDarkTheme)}
+          >テーマを変更します</Button>
+        </Wrapper>
+      </ThemeProvider>
+    </>
   );
 }
 
---
+++ src/theme.ts
@@ -0,0 +1,43 @@
+import { DefaultTheme } from "styled-components";
+
+// export const theme: DefaultTheme = {
+//   colors : {
+//     primary: "#3498db",
+//     secondary: "#2ecc71",
+//     text: "#333",
+//     background: "#f4f4f4",
+//   },
+//   fontSize: {
+//     small: "10px",
+//     medium: "16px",
+//     large: "24px"
+//   },
+// };
+
+export const lightTheme: DefaultTheme = {
+  colors : {
+    primary: "#3498db",
+    secondary: "#2ecc71",
+    background: "#eee",
+    text: "#333",
+  },
+  fontSize: {
+    small: "10px",
+    medium: "16px",
+    large: "24px"
+  },
+};
+
+export const darkTheme: DefaultTheme = {
+  colors : {
+    primary: "#2ecc71",
+    secondary: "#3498db",
+    background: "#333",
+    text: "#fff",
+  },
+  fontSize: {
+    small: "10px",
+    medium: "16px",
+    large: "24px"
+  },
+};
