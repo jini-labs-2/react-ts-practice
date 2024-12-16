ce22da238ff4dafce12fd2f188b9411e278bc06b
und-jini <choiyongjin@undershaft.net>
2024-12-16 22:25:46 +0900

    style適用

--- src/components/todo/Todolist.tsx
+++ src/components/todo/Todolist.tsx
@@ -3,19 +3,44 @@ import { TODO } from '../../api/todo.type';
 
 const Todolist = ({todoList}:{todoList: TODO[]}) => {
   return (
-    <div className="todo-list">
-    {todoList ? todoList.map(todo => {
-      return (
-        <div className="todo-item" key={todo.id}>
-          <div className='todo-id'>{todo.id}</div>
-          <div className='todo-user'>{todo.userId}</div>
-          <div className='todo-title'>{todo.title}</div>
-          <div className='todo-completed'>{todo.completed}</div>
-          <div className='todo-action'> abc</div>
+    <>
+      <div className="todo-list">
+        <div className="todo-item-head">
+          <div className='todo-id'>ID</div>
+          <div className='todo-user'>ユーザーID</div>
+          <div className='todo-title'>題目</div>
+          <div className='todo-completed'>完了</div>
+          <div className='todo-action'>アクション</div>
         </div>
-      )
-    }) : null}
-  </div>
+        {todoList ? todoList.map(todo => {
+          return (
+            <div className="todo-item" key={todo.id}>
+              <div className='todo-id'>{todo.id}</div>
+              <div className='todo-user'>{todo.userId}</div>
+              <div className='todo-title'>{todo.title}</div>
+              <div className='todo-completed'>{todo.completed}</div>
+              <div className='todo-action'> abc</div>
+            </div>
+          )
+        }) : null}
+    </div>
+    <style jsx>{`
+      .todo-item-head {
+        display: grid;
+        grid-template-columns: 10% 10% 60% 10% 10%;
+      }
+      .todo-item-head div {
+        text-align: center;
+      }
+      .todo-item {
+        display: grid;
+        grid-template-columns: 10% 10% 60% 10% 10%;
+      }
+      .todo-item div {
+        border-bottom: 1px solid #000;
+        padding: .2rem
+    `}</style>
+    </>
   )
 }
 
--- src/pages/Todo.tsx
+++ src/pages/Todo.tsx
@@ -28,21 +28,49 @@ const Todo = () => {
   }
 
   return (
-    <div className='todo-inner'>
-      <h1>TODO</h1>
-      <div>
-        <input
-          type='text'
-          value={inputValue}
-          onChange={inputHandle}
-          placeholder='やる事を入力して下さい。'/>
-        <input
-          type='button'
-          value='追加'
-          onClick={addHandle} />
+    <>
+      <div className='todo-inner'>
+        <div className='todo-inner-title'>
+          <h1>TODO</h1>
+        </div>
+        <div className='todo-inner-input'>
+          <input
+            type='text'
+            value={inputValue}
+            onChange={inputHandle}
+            placeholder='やる事を入力して下さい。'/>
+          <input
+            type='button'
+            value='追加'
+            onClick={addHandle} />
+        </div>
+        <Todolist todoList={todos}/>
       </div>
-      <Todolist todoList={todos}/>
-    </div>
+      <style jsx>{`
+        .todo-inner {
+          width: 800px;
+          height: 100vh;
+          background-color: #eee;
+          margin: 0 auto;
+        }
+        .todo-inner-title {
+          text-align: center;
+          padding: 1rem;
+        }
+        .todo-inner-input {
+          text-align: center;
+          margin-bottom: 2rem;
+        }
+        .todo-inner-input input:nth-of-type(1){
+          width: 420px;
+          padding: .2rem;
+        }
+        .todo-inner-input input:nth-of-type(2){
+          width: 60px;
+          padding: 0 .2rem;
+        }
+      `}</style>
+    </>
   )
 }
 
