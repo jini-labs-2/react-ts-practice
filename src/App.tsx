c324c59ec31b2f94b757dd840eb5de470c0ba4ce
und-jini <choiyongjin@undershaft.net>
2025-02-04 08:33:53 +0900

    provider 設定移動

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,19 @@
 import React from 'react';
-import { Provider } from 'react-redux';
 import Todo from './pages/Todo';
-import store from './stores/store';
 import Counter from './pages/Counter';
 import User from './pages/User';
 import Career from './pages/Career';
 function App() {
   return (
-    <Provider store={store}>
-      <div className='app'>
-        <Counter />
-        <hr />
-        <Todo />
-        <hr />
-        <User />
-        <hr />
-        <Career />
-      </div>
-    </Provider>
+    <div className='app'>
+      <Counter />
+      <hr />
+      <Todo />
+      <hr />
+      <User />
+      <hr />
+      <Career />
+    </div>
   );
 }
 
