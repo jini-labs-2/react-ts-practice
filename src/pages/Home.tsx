48c969972bf461cd1041b9a9d8ff0070fbbe6bd0
jini-labs <twonine202109@gmail.com>
2025-02-13 14:10:13 +0900

    bad case

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,51 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
+interface User {
+  name: string;
+  age: string;
+};
+
+function fetchDataFromServer(value: string) {
+  console.log("fetch data.!");
+  const users: User[] = [
+    { name: "tanaka", age: "52" },
+    { name: "sato", age: "48" },
+    { name: "saito", age: "39" },
+    { name: "kinosita", age: "51" },
+    { name: "suzuki", age: "50" },
+  ];
+
+  return users.filter(user => user.name.includes(value))
+}
 
 const Home = () => {
+  const [input, setInput] = useState("");
+  const [fetched, setFetched] = useState<User[]>([]);
+
+  useEffect(() => {
+    const users = fetchDataFromServer(input);
+    setFetched(users);
+  }, [input])
+  
   return (
-    <div>
-     <h2 style={{textAlign: 'center'}}>Home</h2>  
+    <div className='container'>
+      <div className='search-container'>
+        <input
+          placeholder='検索名前を入力してください。'
+          value={input}
+          onChange={(event) => setInput(event.target.value)}
+        />
+        <ul>
+          {fetched.map((user) => (
+            <li key={user.name}>
+              <span> {user.name} </span>
+              <span> {user.age} </span>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Home
