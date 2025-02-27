c062420ff7d221978402694c020d8bc1690ad944
jini-labs <twonine202109@gmail.com>
2025-02-28 00:20:36 +0900

    [useForm] useFormを利用(react-hook-formから)

--- src/pages/testhooks/UseForm.tsx
+++ src/pages/testhooks/UseForm.tsx
@@ -1,56 +1,60 @@
 import React, { useState } from 'react'
+import { useForm } from 'react-hook-form';
+
+interface User {
+  id: number;
+  name: string;
+  sername: string; 
+  email: string;
+}
 
 interface UserFormData {
   username: string;
   useremail: string;
-  userage: number | undefined;
+  userage?: number
 }
 
 const UseForm = () => {
-  const [userFormData, setUserFormData] = useState<UserFormData>();
-  
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value, type } = e.target;
-    const newData = {
-      ...userFormData,
-      [name]: type === 'number' ? parseInt(value) : value
-    } as UserFormData;
-    setUserFormData(newData);
-  }
-
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
+  const [fetchedUser, setFetchedUser] = useState<User>();
+  const {register, handleSubmit, formState: { errors }} = useForm<UserFormData>();
+  const onSubmit = async (data: UserFormData) => { 
     try {
+      console.log(data); 
       const response = await fetch('https://jsonplaceholder.typicode.com/users/1');
-      const data = await response.json();
-      console.log('fetch result:', data)
-    } catch (err) {
-      console.log('error : ', err);
+      const userDetail = await response.json();
+      setFetchedUser(userDetail);
+    } catch(err) {
+      console.error(err); 
     }
-  }
+    }
 
   return (
     <div>
       <h2> useForm example</h2>
       <br/>
       <div>
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div>
             <div>
               <label>Username:</label>
-              <input type='text' name='username' value={userFormData?.username} onChange={handleChange} />
+              <input type="text" {...register('username', { required: true })} />
+              {errors.username && <span style={{color:'red'}}>Username is required</span>}
             </div>
             <div>
               <label>User Email:</label>
-              <input type='text' name='useremail' value={userFormData?.useremail} onChange={handleChange} />
+              <input type='text' {...register('useremail', { required: true })}/>
+              {errors.useremail && <span style={{color:'red'}}>user name is required</span>}
             </div>
             <div>
               <label>User Age:</label>
-              <input type='number' name='userage' value={userFormData?.userage} onChange={handleChange} />
+              <input type='number' {...register('userage', { required: false })}/>
             </div>
           </div>
           <input type='submit' value='確認' />
         </form>
+      </div>
+      <div>
+        {fetchedUser ? JSON.stringify(fetchedUser) : ''}
       </div>
     </div>
   )
