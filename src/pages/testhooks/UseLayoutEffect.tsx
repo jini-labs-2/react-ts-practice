c3878957f7d27bb50ece0d261113ae2405fe2af0
jini-labs <twonine202109@gmail.com>
2025-02-28 17:05:51 +0900

    [useLayoutEffect] 要素の幅を算出、routerバージョンアップ

--- src/pages/testhooks/UseLayoutEffect.tsx
+++ src/pages/testhooks/UseLayoutEffect.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useEffect, useLayoutEffect, useRef, useState } from 'react'
 
 const UseLayoutEffect = () => {
+  const refLabel = useRef<HTMLLabelElement>(null);
+  const [divWidth, setDivWidth] = useState(0);  
+  const [inputValue, setInputValue] = useState<string>('');  
+
+  // useEffect(() => {
+  //   if (refLabel.current) {
+  //     setDivWidth(refLabel.current.getBoundingClientRect().width);
+  //   }
+  //   console.log('--s0201--')
+  // }, [inputValue]);
+
+  useLayoutEffect(() => {
+    if (refLabel.current) {
+      setDivWidth(refLabel.current.getBoundingClientRect().width);
+    }
+    console.log('--s0201--')
+  }, [inputValue]);
+
+  console.log('--s0202--')
   return (
-    <div>UseLayoutEffect</div>
-  )
+    <>
+      <input type='text' value={inputValue} onChange={(e) => setInputValue(e.target.value)} /><br/>
+      <label ref={refLabel}>この要素の幅は{divWidth}pxです.入力値は{inputValue}です</label>
+    </>
+  );
 }
 
 export default UseLayoutEffect
